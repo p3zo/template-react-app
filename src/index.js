3a2a13b0878d798c5049bd9cb122a44b985754bc
p3zo <p3zo@users.noreply.github.com>
2024-08-02 00:40:24 -0700

    Upgrade dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from 'react-dom/client';
 
 import App from "./components/App/App";
 
-const Main = <App />;
-
-ReactDOM.render(Main, document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
+root.render(<App/>);
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,31 @@
-var webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: path.resolve(__dirname, './src/index.js'),
     module: {
         rules: [
             {
-                test: /\.js?$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                loader: "babel-loader",
+                use: ['babel-loader'],
             },
             {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                loader: "style-loader!css-loader",
-            },
-            {
-                test: /\.(?:jpg|png|mp3)$/,
-                exclude: /node_modules/,
-                loader: "file-loader",
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
             },
         ],
     },
     resolve: {
-        extensions: [".js", ".css", ".jpg", ".mp3"],
+        extensions: ['*', '.js', '.jsx'],
     },
     output: {
-        path: __dirname + "/dist",
-        publicPath: "/",
-        filename: "bundle.js",
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
     },
+
+    plugins: [new webpack.HotModuleReplacementPlugin()],
     devServer: {
-        host: "0.0.0.0",
-        port: 8080,
-        contentBase: __dirname + "/dist",
-        hot: true,
-        compress: true,
+        static: path.resolve(__dirname, './dist'),
     },
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": {
-                MY_ENV_VAR: JSON.stringify("my_env_var"),
-            },
-        }),
-    ],
 };
